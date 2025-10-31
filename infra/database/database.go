84cc93798ca98a0b28d27816a2ae6c493a2c1372
Walter Fabian Rodriguez Salazar <wfabianrs@gmail.com>
2025-10-31 15:25:20 -0500

    feat(database): Agregada funcionalidad para la definición base de conexión a BD

---
+++ infra/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+)
+
+// Row representa un registro de la base de datos
+type Row interface {
+	Scan(dest ...any) error
+}
+
+// Rows representa un conjunto de registros
+type Rows interface {
+	Next() bool
+	Scan(dest ...any) error
+	Close()
+}
+
+// DB define operaciones básicas que puede usar la capa de negocio
+type Database interface {
+	Close()
+	Query(ctx context.Context, query string, args ...any) (Rows, error)
+	QueryRow(ctx context.Context, query string, args ...any) Row
+	Exec(ctx context.Context, query string, args ...any) (int64, error)
+
+	// Transacciones
+	BeginTx(ctx context.Context) (Tx, error)
+
+	// Devuelve la conexión 'en crudo' para que pueda ser usada para operaciones no soportadas por la capa de negocio
+	RawConnection() any
+}
+
+// Tx representa una transacción
+type Tx interface {
+	Query(ctx context.Context, query string, args ...any) (Rows, error)
+	QueryRow(ctx context.Context, query string, args ...any) Row
+	Exec(ctx context.Context, query string, args ...any) (int64, error)
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+}
